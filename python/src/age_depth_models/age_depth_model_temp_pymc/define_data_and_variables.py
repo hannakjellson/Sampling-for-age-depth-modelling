5d01435ff0596efad1b66328b794a9321a2968aa
hannakjellson <hannakjellson01@gmail.com>
2025-12-01 18:04:52 +0100

    pymc temperature implementation

---
+++ python/src/age_depth_models/age_depth_model_temp_pymc/define_data_and_variables.py
@@ -0,0 +1,160 @@
+import numpy as np
+import pandas as pd
+import os
+
+def read_data(data):
+    base_path = "../../../../data/"
+    D18O_timeseries = None
+
+    if data.lower() == "dayu06":
+        df = pd.read_csv(
+            os.path.join(base_path, "inputdata_250306A/Dayu cave.txt"), sep="\t"
+        )
+        D18O_timeseries = pd.read_csv(
+            os.path.join(base_path, "inputdata_250306A/d18O_timeseries.txt"), sep="\t"
+        )
+
+    elif data.lower() == "dayu07":
+        df = pd.read_csv(
+            os.path.join(base_path, "inputdata_250307A/Dayu cave.txt"), sep="\t"
+        )
+        D18O_timeseries = pd.read_csv(
+            os.path.join(base_path, "inputdata_250307A/d18O_timeseries.txt"), sep="\t"
+        )
+    elif data.lower() == "dayu26":
+        df = pd.read_csv(
+            os.path.join(base_path, "inputdata_250826/Dayu cave.txt"), sep="\t"
+        )
+        D18O_timeseries = pd.read_csv(
+            os.path.join(base_path, "inputdata_250826/d18O_timeseries.txt"), sep="\t"
+        )
+
+    elif data.lower() == "shenqi":
+        df = pd.read_csv(os.path.join(base_path, "Shenqi cave.txt"), sep="\t")
+
+    df.columns = df.columns.str.replace("%", "").str.strip()
+    if D18O_timeseries is not None:
+        D18O_timeseries.columns = D18O_timeseries.columns.str.replace(
+            "%", ""
+        ).str.strip()
+
+    depths = df["depth"].to_numpy()
+    c14_ages = df["cal_c14_age"].to_numpy()
+    c14_sigma = df["sigma_age"].to_numpy()
+    true_ages = df["true_age"].to_numpy()
+    D18O = df["d18O"].to_numpy()
+    D18O_sigma = df["sigma_d18O"].to_numpy()
+
+    D18O_reference_times = (
+        D18O_timeseries["Year"].to_numpy() if D18O_timeseries is not None else None
+    )
+    D18O_reference = (
+        D18O_timeseries["d18O"].to_numpy() if D18O_timeseries is not None else None
+    )
+
+    return (
+        depths,
+        c14_ages,
+        c14_sigma,
+        true_ages,
+        D18O,
+        D18O_sigma,
+        D18O_reference_times,
+        D18O_reference,
+    )
+
+
+def get_data():
+    (
+        depths,
+        c14_ages,
+        c14_sigma,
+        true_ages,
+        D18O,
+        D18O_sigma,
+        D18O_reference_times,
+        D18O_reference,
+    ) = read_data("dayu06")
+    c14_mask = ~np.isnan(c14_ages)
+    D18O_mask = ~np.isnan(D18O)
+
+    c14_depths = depths[c14_mask]
+    c14_ages = c14_ages[c14_mask]
+    c14_sigma = c14_sigma[c14_mask]
+
+    D18O = D18O[D18O_mask][::-1]
+    D18O_sigma = D18O_sigma[D18O_mask][::-1]
+    D18O_depths = depths[D18O_mask][
+        ::-1
+    ]  # This does not overlap with the c14 depths in the file.
+    true_ages_d18O = true_ages[D18O_mask][::-1]
+
+    data = {
+        "theta": true_ages[0],
+        "c14_ages": c14_ages,
+        "c14_depths": c14_depths,
+        "c14_sigma": c14_sigma,
+        "num_c14_depths": len(c14_depths),
+        "d18O": D18O,
+        "d18O_sigma": D18O_sigma,
+        "d18O_depths": D18O_depths,
+        "d18O_reference_times": D18O_reference_times,
+        "d18O_reference": D18O_reference,
+        "num_D18O_depths": len(D18O_depths),
+        "num_D18O_reference_times": len(D18O_reference_times),
+        "true_ages_D18O": true_ages_d18O,
+    }
+
+    return data
+
+
+def get_hmc_config():
+    N = 50
+    H = 100
+    delta_c = H / N
+    cs = np.linspace(0, H, N + 1)
+    num_samples = 1000
+    num_chains = 4
+    a = 1.5
+    b = 0.27
+    dt = 0.001
+    nt = 20
+    mt = 3
+    ndt = 700
+    nhmc = 1
+    dE = 50
+
+
+    config = {
+        "N": N,
+        "H": H,
+        "delta_c": delta_c,
+        "cs": cs,
+        "num_samples": num_samples,
+        "num_chains": num_chains,
+        "a": a,
+        "b": b,
+        "dt" : dt,
+        "mt" : mt,
+        "nt" : nt,
+        "ndt" : ndt,
+        "nhmc" : nhmc,
+        "dE" : dE,
+    }
+
+    special_key = "rsp"
+
+    parts = []
+    for k, v in config.items():
+        if k == special_key:
+            continue
+        if isinstance(v, bool):
+            if v:  # only include if True
+                parts.append(f"{k}")
+        elif isinstance(v, float):
+            parts.append(f"{k}{v:.2g}")
+        elif isinstance(v, int):
+            parts.append(f"{k}{v}")
+    config_str = "_".join(parts)
+
+    return config, config_str
